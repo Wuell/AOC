8f5578e80ea95f951e10f88aa7e7a22f97419c41
SNesht <101153084+SNesht@users.noreply.github.com>
2023-07-09 23:13:32 -0300

    fill pronto

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -11,3 +11,134 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
+//while (1):
+(LOOP)
+//    if key.isPressed() goto black:
+@I
+M = 0
+@KBD
+D = M
+@BLACK
+D;JNE
+//    else goto white:
+@WHITE
+0;JMP 
+
+(BLACK)
+// for i in range(len(screen))
+@I
+D = M
+@1030
+D = D - A
+@END
+D;JEQ
+// OQUE VAI SER RODADO VARIAS VEZES
+// screen[i] = -1;
+//IDA
+@I
+D = M
+@SCREEN
+A = A + D
+M = -1
+@I
+D = M
+@17407
+A = A + D
+M = -1
+@I
+D = M
+@18430
+A = A + D
+M = -1
+@I
+D = M
+@19453
+A = A + D
+M = -1
+// VOLTA
+@I
+D = M
+@24575
+A = A - D
+M = -1
+@I
+D = M
+@23552
+A = A - D
+M = -1
+@I
+D = M
+@22529
+A = A - D
+M = -1
+@I
+D = M
+@21506
+A = A - D
+M = -1
+
+// INCREMENDO DO I
+@I
+M = M + 1
+@BLACK
+0;JMP
+
+// for i in range(len(screen))
+(WHITE)
+@I
+D = M
+@1030
+D = D - A
+@END
+D;JEQ
+// screen[i] = 0;
+@I
+D = M
+@SCREEN
+A = A + D
+M = 0
+@I
+D = M
+@17407
+A = A + D
+M = 0
+@I
+D = M
+@18430
+A = A + D
+M = 0
+@I
+D = M
+@19453
+A = A + D
+M = 0
+// VOLTA
+@I
+D = M
+@24575
+A = A - D
+M = 0
+@I
+D = M
+@23552
+A = A - D
+M = 0
+@I
+D = M
+@22529
+A = A - D
+M = 0
+@I
+D = M
+@21506
+A = A - D
+M = 0
+//INCREMENTO DO I
+@I
+M = M + 1
+@WHITE
+0;JMP
+
+(END)
+@LOOP
+0;JMP
